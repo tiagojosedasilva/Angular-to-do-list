3cece171accf609f95b4e175e33d4b9aa354d001
tiagojosedasilva <tiago.estudo.desenvolvimento@gmail.com>
2023-07-20 16:39:29 -0300

    criando as telas do App e método para deletar

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+//interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class TodoListComponent {
 
+  public taskList: Array<TaskList> = [
+    {task: "Minha nova task", checked: true},
+    {task: "Minha nova task 2", checked: false}
+  ];
+
+  ngOnInit(): void{}
+
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Tem certeza que deseja deletar tudo?")
+
+    if(confirm){
+      this.taskList = [];
+    }
+  }
 }
